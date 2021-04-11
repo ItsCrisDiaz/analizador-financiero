8f7f21aa41b2875a25d144f0dbe30999dd47a629
Cristian Díaz <crisdi384@hotmail.com>
2021-04-10 21:24:57 -0500

    Adding modules addClassAndMessage and showIndicator

--- js/index.js
+++ js/index.js
@@ -60,70 +60,7 @@ import createSummary from "./createSummary.js";
 
 import changeSectionButton from "./changeSectionButton.js";
 
-import calculation from "./calculation.js";
-
-import caseIndicator from "./caseIndicator.js";
-
-const addClassAndMessage = (indicator, result) => {
-  let indicatorNode = document.querySelector(
-    `#${INDICATORS_MESSAGES[indicator].id} .indicator`
-  );
-  let indicatorStateNode = document.querySelector(
-    `#${INDICATORS_MESSAGES[indicator].id} .indicator-state`
-  );
-  let indicatorStateExplanation = document.querySelector(
-    `#${INDICATORS_MESSAGES[indicator].id} .indicator-explanation`
-  );
-  let indicatorCalculation = calculation(indicator);
-  let caseIndicatorResult = caseIndicator(indicator, result);
-
-  if (indicatorCalculation === "N/A") {
-    indicatorNode.classList.add("warning");
-    indicatorNode.classList.remove("correct", "wrong");
-    indicatorStateNode.innerHTML = "No aplicable a tus estados";
-    indicatorStateNode.classList.add("warning");
-    indicatorStateNode.classList.remove("correct", "wrong");
-    indicatorStateExplanation.innerHTML =
-      "No es posible calcular este indicador financiero con base en la información brindada.";
-  } else if (caseIndicatorResult === "positive") {
-    indicatorNode.classList.add("correct");
-    indicatorNode.classList.remove("wrong", "warning");
-    indicatorStateNode.innerHTML = `${INDICATORS_MESSAGES[indicator].positiveText}`;
-    indicatorStateNode.classList.add("correct");
-    indicatorStateNode.classList.remove("wrong", "warning");
-    indicatorStateExplanation.innerHTML = `${INDICATORS_MESSAGES[indicator].positiveMessage}`;
-  } else if (caseIndicatorResult === "negative") {
-    indicatorNode.classList.add("wrong");
-    indicatorNode.classList.remove("correct", "warning");
-    indicatorStateNode.innerHTML = `${INDICATORS_MESSAGES[indicator].negativeText}`;
-    indicatorStateNode.classList.add("wrong");
-    indicatorStateNode.classList.remove("correct", "warning");
-    indicatorStateExplanation.innerHTML = `${INDICATORS_MESSAGES[indicator].negativeMessage}`;
-  } else {
-    indicatorNode.classList.add("warning");
-    indicatorNode.classList.remove("correct", "wrong");
-    indicatorStateNode.innerHTML = `${INDICATORS_MESSAGES[indicator].warningText}`;
-    indicatorStateNode.classList.add("warning");
-    indicatorStateNode.classList.remove("correct", "wrong");
-    indicatorStateExplanation.innerHTML = `${INDICATORS_MESSAGES[indicator].warningMessage}`;
-  }
-};
-
-const showIndicator = function (indicator) {
-  let indicatorCalculation = calculation(indicator);
-  let indicatorNode = document.querySelector(
-    `#${INDICATORS_MESSAGES[indicator].id} .indicator`
-  );
-  if (indicatorCalculation === "N/A") {
-    indicatorNode.innerHTML = `${indicatorCalculation}`;
-  } else if (INDICATORS_MESSAGES[indicator].percentage === true) {
-    indicatorNode.innerHTML = `${indicatorCalculation}%`;
-  } else {
-    indicatorNode.innerHTML = `${indicatorCalculation}`;
-  }
-
-  addClassAndMessage(indicator, indicatorCalculation);
-};
+import showIndicator from "./showIndicator.js";
 
 //########################################
 //EVENT LISTENERS
