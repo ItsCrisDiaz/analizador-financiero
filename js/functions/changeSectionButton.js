5476d681fd3f00137a7c0dd14ee81f5e515bb109
Cristian Díaz <crisdi384@hotmail.com>
2021-04-11 19:30:36 -0500

    *Adición de h2 en la sección de summary para activar el método de pushState * Adición de método history.pushState en función changeSectionButton

--- js/functions/changeSectionButton.js
+++ js/functions/changeSectionButton.js
@@ -5,7 +5,12 @@ const changeSectionButton = function (hiddenSectionTrue, hiddenSectionFalse) {
   //Shows the node in the parameter hiddenSectionFalse
   hiddenSectionFalse.hidden = false;
 
-  window.location = `#${hiddenSectionFalse.id}`;
+  let pageUrl = `${hiddenSectionFalse.id}.html`;
+
+  let pageTitle = hiddenSectionFalse.querySelector("h2").innerHTML;
+
+  history.pushState({ page_id: 1 }, pageTitle, pageUrl);
+  document.title = `${pageTitle}`;
 };
 
 export default changeSectionButton;
