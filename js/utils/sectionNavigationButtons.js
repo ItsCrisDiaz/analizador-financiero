41f5878477f936500c0e374d09d9a0a658ca6665
Cristian Díaz <crisdi384@hotmail.com>
2021-05-01 10:20:03 -0500

    *Removed max-width for modal ul[list] elements

--- js/utils/sectionNavigationButtons.js
+++ js/utils/sectionNavigationButtons.js
@@ -52,7 +52,12 @@ const SUBMIT_FORM_BUTTON = document.querySelector("#generateContent");
 // * @param {node} hiddenSectionFalse  Node with the section that needs to be showed
 // */
 
-const changeSectionButton = function (hiddenSectionTrue, hiddenSectionFalse) {
+const changeSectionButton = function (
+  hiddenSectionTrue,
+  hiddenSectionFalse,
+  buttonContainerCondition
+) {
+  debugger;
   let buttonContainerSectionTrue = document.querySelector(
     `#${hiddenSectionTrue.id}Button`
   );
@@ -64,10 +69,10 @@ const changeSectionButton = function (hiddenSectionTrue, hiddenSectionFalse) {
   hiddenSectionFalse.hidden = false;
 
   hiddenSectionTrue.classList.remove("section-on-screen");
-  hiddenSectionFalse.classList.add("section-on-screen");
-
   buttonContainerSectionTrue.classList.remove("buttons-on-screen");
   buttonContainerSectionTrue.hidden = true;
+
+  hiddenSectionFalse.classList.add("section-on-screen");
   buttonContainerSectionFalse.classList.add("buttons-on-screen");
   buttonContainerSectionFalse.hidden = false;
 
@@ -86,33 +91,39 @@ const changeSectionButton = function (hiddenSectionTrue, hiddenSectionFalse) {
 const buttonEventListeners = function () {
   document.addEventListener("click", function (e) {
     if (e.target === INSTRUCTION_BUTTON) {
-      changeSectionButton(INSTRUCTIONS_SECTION, ASSETS_SECTION);
+      changeSectionButton(INSTRUCTIONS_SECTION, ASSETS_SECTION, true);
     } else if (e.target === ASSETS_BACK_BUTTON) {
-      changeSectionButton(ASSETS_SECTION, INSTRUCTIONS_SECTION);
+      changeSectionButton(ASSETS_SECTION, INSTRUCTIONS_SECTION, true);
     } else if (e.target === ASSETS_NEXT_BUTTON) {
-      changeSectionButton(ASSETS_SECTION, LIABILITIES_AND_EQUITY_SECTION);
+      changeSectionButton(ASSETS_SECTION, LIABILITIES_AND_EQUITY_SECTION, true);
     } else if (e.target === LIABILITIES_AND_EQUITY_BACK_BUTTON) {
-      changeSectionButton(LIABILITIES_AND_EQUITY_SECTION, ASSETS_SECTION);
+      changeSectionButton(LIABILITIES_AND_EQUITY_SECTION, ASSETS_SECTION, true);
     } else if (e.target === LIABILITIES_AND_EQUITY_NEXT_BUTTON) {
       changeSectionButton(
         LIABILITIES_AND_EQUITY_SECTION,
-        PROFIT_AND_LOSS_SECTION
+        PROFIT_AND_LOSS_SECTION,
+        true
       );
     } else if (e.target === PROFIT_AND_LOSS_BACK_BUTTON) {
       changeSectionButton(
         PROFIT_AND_LOSS_SECTION,
-        LIABILITIES_AND_EQUITY_SECTION
+        LIABILITIES_AND_EQUITY_SECTION,
+        true
       );
     } else if (e.target === PROFIT_AND_LOSS_NEXT_BUTTON) {
-      changeSectionButton(PROFIT_AND_LOSS_SECTION, SUMMARY_SECTION);
+      changeSectionButton(PROFIT_AND_LOSS_SECTION, SUMMARY_SECTION, true);
     } else if (e.target === SUMMARY_ASSETS_BUTTON) {
-      changeSectionButton(SUMMARY_SECTION, ASSETS_SECTION);
-    } else if (e.target === SUMMARY_LIABILITIES_AND_EQUITY_BUTTON) {
-      changeSectionButton(SUMMARY_SECTION, LIABILITIES_AND_EQUITY_SECTION);
+      changeSectionButton(SUMMARY_SECTION, ASSETS_SECTION, true);
+    } else if ((e.target === SUMMARY_LIABILITIES_AND_EQUITY_BUTTON, true)) {
+      changeSectionButton(
+        SUMMARY_SECTION,
+        LIABILITIES_AND_EQUITY_SECTION,
+        true
+      );
     } else if (e.target === SUMMARY_PROFIT_AND_LOSS_BUTTON) {
-      changeSectionButton(SUMMARY_SECTION, PROFIT_AND_LOSS_SECTION);
+      changeSectionButton(SUMMARY_SECTION, PROFIT_AND_LOSS_SECTION, true);
     } else if (e.target === SUBMIT_FORM_BUTTON) {
-      changeSectionButton(SUMMARY_SECTION, REPORT_SECTION);
+      changeSectionButton(SUMMARY_SECTION, REPORT_SECTION, false);
     }
   });
 };
